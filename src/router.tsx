2cb22af98c9db47489fa3f58fb89dde76d6d426f
Robert <ludw1gj@icloud.com>
2022-11-13 20:53:51 +1100

    Add 404 page

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import GameOfLifeView from "./views/GameOfLifeView";
 import HomeView from "./views/HomeView";
+import NotFoundView from "./views/NotFoundView";
 
 function AppRouter() {
   return (
@@ -9,6 +10,7 @@ function AppRouter() {
       <Routes>
         <Route path="/" element={<HomeView />} />
         <Route path="/game-of-life" element={<GameOfLifeView />} />
+        <Route path="*" element={<NotFoundView />} />
       </Routes>
     </Suspense>
   );
