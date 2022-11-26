dbc598144dc0fd1cfa48b6fb658b4922fe77f34b
Robert <ludw1gj@icloud.com>
2022-11-26 16:27:34 +1100

    Migrate to Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router";
-import { ChakraProvider } from "@chakra-ui/react";
 import HeaderBar from "./components/HeaderBar";
 import { Header, Main } from "./StyledApp";
 
 function App() {
   return (
-    <ChakraProvider>
-      <BrowserRouter>
-        <Header>
-          <HeaderBar />
-        </Header>
-        <Main>
-          <AppRouter />
-        </Main>
-      </BrowserRouter>
-    </ChakraProvider>
+    <BrowserRouter>
+      <Header>
+        <HeaderBar />
+      </Header>
+      <Main>
+        <AppRouter />
+      </Main>
+    </BrowserRouter>
   );
 }
 
